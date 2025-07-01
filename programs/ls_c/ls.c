e2c939ce72081b142b12175a3bb956ae862292ea
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2025-07-01 11:21:19 -0700

    feat: wrote ls in C

---
+++ programs/ls_c/ls.c
@@ -0,0 +1,36 @@
+// This program is a copy of 'ls'
+//
+// compile with:
+// clang -o ls_c ls.c
+//
+// run with:
+// ./ls_c
+
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  DIR *dp;
+  struct dirent *dirp;
+  char *path;
+
+  if (argc == 1) {
+    path = ".";
+  } else if (argc == 2) {
+    path = argv[1];
+  }
+
+  dp = opendir(path);
+  if (dp == NULL) {
+    perror("opendir");
+    exit(EXIT_FAILURE);
+  }
+
+  while ((dirp = readdir(dp)) != NULL)
+    puts(dirp->d_name);
+
+  closedir(dp);
+  return 0;
+}
